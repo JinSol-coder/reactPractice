660e8e46e1452fa6cae487222c8e191e3a5760fc
IM_SORRY <imwe25@naver.com>
2024-12-24 09:38:50 +0900

    first

---
+++ src/components/Controller.jsx
@@ -0,0 +1,26 @@
+const Controller =({onClickCount})=>{
+    return(
+        <div>
+            <button onClick={()=>{
+                onClickCount(-100)
+            }}>-100</button>
+            <button onClick={()=>{
+                onClickCount(-10)
+            }}>-10</button>
+            <button onClick={()=>{
+                onClickCount(-1)
+            }}>-1</button>
+            <button onClick={()=>{
+                onClickCount(+1)
+            }}>+1</button>
+            <button onClick={()=>{
+                onClickCount(+10)
+            }}>+10</button>
+            <button onClick={()=>{
+                onClickCount(+100)
+            }}>+100</button>
+        </div>
+    );
+};
+
+export default Controller;
